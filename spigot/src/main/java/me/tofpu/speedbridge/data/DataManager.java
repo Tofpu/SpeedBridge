36f2bfc8a6b558e786554538795cbf798373f62a
Tofpu <47629321+Tofpu@users.noreply.github.com>
2021-09-12 08:58:20 +0200

    removal of the old leaderboard implementation

--- spigot/src/main/java/me/tofpu/speedbridge/data/DataManager.java
+++ spigot/src/main/java/me/tofpu/speedbridge/data/DataManager.java
@@ -4,15 +4,12 @@ import com.github.requestpluginsforfree.config.ConfigAPI;
 import com.github.requestpluginsforfree.fileutil.file.PluginFile;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
-import com.google.gson.reflect.TypeToken;
 import me.tofpu.speedbridge.api.island.Island;
 import me.tofpu.speedbridge.api.island.IslandService;
-import me.tofpu.speedbridge.api.lobby.BoardUser;
 import me.tofpu.speedbridge.api.lobby.LobbyService;
 import me.tofpu.speedbridge.api.user.User;
 import me.tofpu.speedbridge.api.user.UserService;
 import me.tofpu.speedbridge.data.adapter.IslandAdapter;
-import me.tofpu.speedbridge.data.adapter.LeaderboardTypeAdapter;
 import me.tofpu.speedbridge.data.adapter.LocationAdapter;
 import me.tofpu.speedbridge.data.adapter.UserAdapter;
 import me.tofpu.speedbridge.data.file.path.Path;
@@ -27,7 +24,6 @@ import org.bukkit.plugin.Plugin;
 
 import java.io.File;
 import java.io.IOException;
-import java.util.List;
 import java.util.Locale;
 import java.util.UUID;
 
@@ -36,13 +32,11 @@ public class DataManager {
             .registerTypeAdapter(Location.class, new LocationAdapter())
             .registerTypeAdapter(Island.class, new IslandAdapter())
             .registerTypeAdapter(User.class, new UserAdapter())
-            .registerTypeAdapter(
-                    new TypeToken<List<BoardUser>>() {}.getType(),
-                    new LeaderboardTypeAdapter())
             .setPrettyPrinting()
             .serializeNulls()
             .create();
 
+    private final Plugin plugin;
     private final File[] files;
     private final PluginFile[] pluginFiles;
 
@@ -50,7 +44,8 @@ public class DataManager {
     private UserServiceImpl userService;
     private LobbyService lobbyService;
 
-    public DataManager() {
+    public DataManager(final Plugin plugin) {
+        this.plugin = plugin;
         this.files = new File[5];
         this.pluginFiles = new PluginFile[2];
     }
@@ -63,8 +58,9 @@ public class DataManager {
         this.files[0] = parentDirectory;
         this.files[1] = new File(parentDirectory, "islands");
         this.files[2] = new File(parentDirectory, "users");
-        this.files[3] = new File(parentDirectory, "lobby.json");
-        this.files[4] = new File(parentDirectory, "leaderboard.json");
+        this.files[3] = new File(parentDirectory, "leaderboards");
+        this.files[4] = new File(parentDirectory, "lobby.json");
+//        this.files[4] = new File(parentDirectory, "leaderboard.json");
 
         this.pluginFiles[0] = new SettingsFile(plugin, parentDirectory);
         this.pluginFiles[1] = new MessageFile(plugin, parentDirectory);
@@ -124,15 +120,15 @@ public class DataManager {
         userService.removeUser(user);
     }
 
-    public void load() {
-        lobbyService.load(GSON, files[3], files[4]);
+    public void load() throws IOException {
+        lobbyService.load(GSON, files[4]);
         islandService.loadAll();
     }
 
     public void shutdown() {
         islandService.saveAll(true);
         userService.saveAll(true);
-        lobbyService.save(GSON, files[3], files[4]);
+        lobbyService.save(GSON, files[4]);
     }
 
     public File[] getFiles() {
