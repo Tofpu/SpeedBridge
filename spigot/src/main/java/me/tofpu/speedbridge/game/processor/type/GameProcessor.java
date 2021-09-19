07dc525dc4712edb1ae6e3f8251b6551df36016b
Tofpu <47629321+Tofpu@users.noreply.github.com>
2021-09-19 04:12:22 +0200

    processors for spectator + item

--- spigot/src/main/java/me/tofpu/speedbridge/game/processor/type/GameProcessor.java
+++ spigot/src/main/java/me/tofpu/speedbridge/game/processor/type/GameProcessor.java
@@ -3,6 +3,7 @@ package me.tofpu.speedbridge.game.processor.type;
 import me.tofpu.speedbridge.api.island.Island;
 import me.tofpu.speedbridge.api.user.User;
 import me.tofpu.speedbridge.game.processor.ProcessType;
+import me.tofpu.speedbridge.game.service.GameServiceImpl;
 import org.bukkit.Location;
 import org.bukkit.entity.Player;
 
@@ -14,13 +15,12 @@ public abstract class GameProcessor {
     public void process(final User user, final Player player,
             final ProcessType type) {}
 
-    public void process(final User user, Island island, final ProcessType type) {
-        process(user, user.player(), island, type);
-    }
-
-    public void process(final User user, final Player player, Island island,
+    public void process(final GameServiceImpl gameService, final User user,
+            final Player player, Island island,
      final ProcessType type) {}
 
-    public void process(final Location location, final ProcessType type,
+    public void process(final GameServiceImpl gameService,
+            final Location location,
+            final ProcessType type,
             final User... users) {}
 }
