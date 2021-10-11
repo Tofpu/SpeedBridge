170c7c7686d2219cc8f872f28e16a1df99ea0ab3
Tofpu <47629321+Tofpu@users.noreply.github.com>
2021-10-12 01:20:52 +0200

    added methods for islands in storage

--- api/src/main/java/me/tofpu/speedbridge/api/model/storage/Storage.java
+++ api/src/main/java/me/tofpu/speedbridge/api/model/storage/Storage.java
@@ -1,15 +1,20 @@
 package me.tofpu.speedbridge.api.model.storage;
 
+import me.tofpu.speedbridge.api.model.object.island.Island;
 import me.tofpu.speedbridge.api.model.object.lobby.Lobby;
 import me.tofpu.speedbridge.api.model.object.user.User;
 
+import java.util.Collection;
 import java.util.Optional;
 import java.util.UUID;
 
 public interface Storage {
     Optional<Lobby> loadLobby();
+    Collection<Island> loadIslands();
     Optional<User> loadUser(final UUID uniqueId);
     void saveLobby();
+    void deleteIsland(final int slot);
+    void saveIslands();
     void saveUser(final User user);
     void saveUsers();
 }
--- spigot/src/main/java/me/tofpu/speedbridge/model/storage/FileStorage.java
+++ spigot/src/main/java/me/tofpu/speedbridge/model/storage/FileStorage.java
@@ -1,8 +1,10 @@
 package me.tofpu.speedbridge.model.storage;
 
 import com.google.inject.Inject;
+import me.tofpu.speedbridge.api.model.object.island.Island;
 import me.tofpu.speedbridge.api.model.object.lobby.Lobby;
 import me.tofpu.speedbridge.api.model.object.user.User;
+import me.tofpu.speedbridge.api.model.repository.IslandRepository;
 import me.tofpu.speedbridge.api.model.repository.LobbyRepository;
 import me.tofpu.speedbridge.api.model.repository.UserRepository;
 import me.tofpu.speedbridge.api.model.storage.Storage;
@@ -12,25 +14,28 @@ import java.io.File;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.util.Optional;
-import java.util.UUID;
+import java.nio.file.Files;
+import java.util.*;
 
 public class FileStorage implements Storage {
-    private final File[] files = new File[3];
+    private final File[] files = new File[4];
 
     private final StorageHelper helper;
     private final LobbyRepository lobbyRepository;
+    private final IslandRepository islandRepository;
     private final UserRepository userRepository;
 
     @Inject
-    public FileStorage(final StorageHelper helper, final Plugin plugin, final LobbyRepository lobbyRepository, final UserRepository userRepository) {
+    public FileStorage(final StorageHelper helper, final Plugin plugin, final LobbyRepository lobbyRepository, final IslandRepository islandRepository, final UserRepository userRepository) {
         this.helper = helper;
         this.lobbyRepository = lobbyRepository;
+        this.islandRepository = islandRepository;
         this.userRepository = userRepository;
 
         this.files[0] = plugin.getDataFolder();
         this.files[1] = new File(files[0], "lobby.json");
-        this.files[2] = new File(files[0], "users");
+        this.files[2] = new File(files[0], "islands");
+        this.files[3] = new File(files[0], "users");
 
         for (final File file : files) {
             if (file.getName().contains(".")) {
@@ -57,8 +62,29 @@ public class FileStorage implements Storage {
     }
 
     @Override
+    public Collection<Island> loadIslands() {
+        final List<Island> islands = new ArrayList<>();
+        for (final File file : files[2].listFiles()) {
+            try {
+                if (!file.getName().endsWith(".json")) continue;
+
+                final Island island;
+                try (final FileReader reader = new FileReader(file)) {
+                    island = helper.gson().fromJson(reader, Island.class);;
+                }
+                if (island == null) continue;
+
+                islands.add(island);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        return islands;
+    }
+
+    @Override
     public Optional<User> loadUser(final UUID uniqueId) {
-        final File file = new File(files[2], uniqueId.toString());
+        final File file = new File(files[3], uniqueId.toString());
         if (!file.exists()) {
             return Optional.empty();
         }
@@ -74,7 +100,7 @@ public class FileStorage implements Storage {
 
     @Override
     public void saveUser(final User user) {
-        final File file = new File(files[2], user.uniqueId().toString());
+        final File file = new File(files[3], user.uniqueId().toString());
         try {
             try (final FileWriter writer = new FileWriter(file, false)) {
                 this.helper.gson().toJson(user, User.class, writer);
@@ -90,6 +116,29 @@ public class FileStorage implements Storage {
             this.helper.gson().toJson(lobbyRepository.lobby(), Lobby.class, writer);
         } catch (IOException e) {
             throw new IllegalStateException(e);
+        }
+    }
+
+    @Override
+    public void deleteIsland(final int slot) {
+        final File islandFile = new File(files[2], "island-" + slot +
+                ".json");
+        try {
+            Files.deleteIfExists(islandFile.toPath());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void saveIslands() {
+        for (final Island island : islandRepository.islands()) {
+            final File file = new File(files[2], island.slot() + "");
+            try (final FileWriter writer = new FileWriter(file, false)) {
+                this.helper.gson().toJson(island, Island.class, writer);
+            } catch (IOException e) {
+                throw new IllegalStateException(e);
+            }
         }
     }
 
